3b0ad3936c918394d87a4efb643a4f2c54fbf0d6
rocaiguina <lrocaiguina@gmail.com>
2018-04-08 23:57:47 -0400

    disabled DEBUG mode

--- wp-content/themes/cpipr/functions.php
+++ wp-content/themes/cpipr/functions.php
@@ -21,7 +21,7 @@ if ( ! defined( 'INN_HOSTED' ) ) {
  * @since 0.5
  */
 if ( ! defined( 'LARGO_DEBUG' ) )
-	define( 'LARGO_DEBUG', TRUE );
+	define( 'LARGO_DEBUG', FALSE );
 
 //use the Largo metabox API
 require_once( get_template_directory() . '/largo-apis.php' );
