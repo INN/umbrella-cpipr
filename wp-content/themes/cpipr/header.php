3498dbf2fd78f9ed64abd97a44e1dff891aaac43
rocaiguina <lrocaiguina@gmail.com>
2018-04-09 15:30:55 -0400

    Deleted unused fonts and remove call to google from header

--- wp-content/themes/cpipr/header.php
+++ wp-content/themes/cpipr/header.php
@@ -8,7 +8,6 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-	<link href="https://fonts.googleapis.com/css?family=Merriweather|Roboto|Roboto+Condensed" rel="stylesheet">
 <?php
 	/**
 	 * The template for displaying the header
