3fead122c48c3afcd7ac16119ba9d15df45a4916
rocaiguina <lrocaiguina@gmail.com>
2018-04-09 15:42:53 -0400

    removed unnecessary meta tags from header

--- wp-content/themes/cpipr/header.php
+++ wp-content/themes/cpipr/header.php
@@ -4,10 +4,7 @@
 <!--[if IE 8]>    <html <?php language_attributes(); ?> class="no-js ie8"> <![endif]-->
 <!--[if IE 9]>    <html <?php language_attributes(); ?> class="no-js ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
 <?php
 	/**
 	 * The template for displaying the header
