1b3d69175b97ab65b3a6fbc881b5d5ccb67d241a
Ben Keith <ben@benlk.com>
2018-04-10 14:19:55 -0400

    Put back the missing charset, remove duplicate </head>

--- wp-content/themes/cpipr/header.php
+++ wp-content/themes/cpipr/header.php
@@ -5,9 +5,9 @@
 <!--[if IE 9]>    <html <?php language_attributes(); ?> class="no-js ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html <?php language_attributes(); ?> class="no-js"> <!--<![endif]-->
 <head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-</head>
 <?php
 	/**
 	 * The template for displaying the header
