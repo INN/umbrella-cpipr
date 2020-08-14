e661af559e6e03100029c2c7d4bd511eeb117378
Jorge Moreira <jmoreira@linkux-it.com>
2020-08-14 13:58:41 -0400

    Fix caption image alignment on single post

--- wp-content/themes/cpipr/partials/content-single.php
+++ wp-content/themes/cpipr/partials/content-single.php
@@ -3,45 +3,46 @@
  * The template for displaying content in the single.php template
  */
 ?>
+<div class="container-fluid">
+	<article id="post-<?php the_ID(); ?>" <?php post_class( 'hnews item' ); ?> itemscope itemtype="http://schema.org/Article">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'hnews item' ); ?> itemscope itemtype="http://schema.org/Article">
+		<?php do_action('largo_before_post_header'); ?>
 
-	<?php do_action('largo_before_post_header'); ?>
+		<header>
 
-	<header>
+			<?php largo_maybe_top_term(); ?>
 
-		<?php largo_maybe_top_term(); ?>
+			<h1 class="entry-title" itemprop="headline"><?php the_title(); ?></h1>
+			<?php if ( $subtitle = get_post_meta( $post->ID, 'subtitle', true ) ) : ?>
+				<h2 class="subtitle"><?php echo $subtitle ?></h2>
+			<?php endif; ?>
+			<h5 class="byline"><?php largo_byline(true, false, $post->ID); ?></h5>
 
-		<h1 class="entry-title" itemprop="headline"><?php the_title(); ?></h1>
-		<?php if ( $subtitle = get_post_meta( $post->ID, 'subtitle', true ) ) : ?>
-			<h2 class="subtitle"><?php echo $subtitle ?></h2>
-		<?php endif; ?>
-		<h5 class="byline"><?php largo_byline(true, false, $post->ID); ?></h5>
+			<?php if ( ! of_get_option( 'single_social_icons' ) == false ) {
+				largo_post_social_links();
+			} ?>
 
-		<?php if ( ! of_get_option( 'single_social_icons' ) == false ) {
-			largo_post_social_links();
-		} ?>
+	<?php largo_post_metadata( $post->ID ); ?>
 
-<?php largo_post_metadata( $post->ID ); ?>
+		</header><!-- / entry header -->
 
-	</header><!-- / entry header -->
+		<?php
+			do_action('largo_after_post_header');
 
-	<?php
-		do_action('largo_after_post_header');
+			largo_hero(null,'');
 
-		largo_hero(null,'');
+			do_action('largo_after_hero');
+		?>
 
-		do_action('largo_after_hero');
-	?>
+		<?php get_sidebar(); ?>
 
-	<?php get_sidebar(); ?>
+		<section class="entry-content clearfix" itemprop="articleBody">
+			
+			<?php largo_entry_content( $post ); ?>
+			
+		</section>
 
-	<section class="entry-content clearfix" itemprop="articleBody">
-		
-		<?php largo_entry_content( $post ); ?>
-		
-	</section>
+		<?php do_action('largo_after_post_content'); ?>
 
-	<?php do_action('largo_after_post_content'); ?>
-
-</article>
+	</article>
+</div>
